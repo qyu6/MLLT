bc4cedf0bf05329c6e8070c10847f5a31cc74eb4
tq <qyutony@163.com>
2022-12-06 15:55:39 +0800

    Auto_refresh_file_modified_timestamp

---
+++ 098_Projects/002_批量更新文件修改时间戳/Run.bat
@@ -0,0 +1,16 @@
+chcp 65001
+setlocal enabledelayedexpansion
+
+for /r "C:\Users\xxx\xxx\folder”%%i in (*) do (
+	echo 1. %%~ti 
+	set str=%%~ti
+	echo 2.!str!
+	set str1=!str:~0,4!
+	echo 3. !str1! 
+	echo 4. %%i
+	if !str1! LEQ 2021 ( 
+	touch"%%i”-c
+	) else (
+	echo 5. not meet touch condition
+	)
+)

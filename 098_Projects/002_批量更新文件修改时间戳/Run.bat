b851fd15f4ba8a4a5f00c00d9ea2acd207243405
tq <qyutony@163.com>
2022-12-09 00:10:27 +0800

    revise

--- 098_Projects/002_批量更新文件修改时间戳/Run.bat
+++ 098_Projects/002_批量更新文件修改时间戳/Run.bat
@@ -1,15 +1,15 @@
 chcp 65001
 setlocal enabledelayedexpansion
 
-for /r "C:\Users\xxx\xxx\folder”%%i in (*) do (
-	echo 1. %%~ti 
+for /r "C:\Users\Tony\Downloads" %%i in (*) do (
+	echo 1. %%~ti
 	set str=%%~ti
-	echo 2.!str!
+	echo 2. !str!
 	set str1=!str:~0,4!
-	echo 3. !str1! 
+	echo 3. !str1!
 	echo 4. %%i
-	if !str1! LEQ 2021 ( 
-	touch"%%i”-c
+	if !str1! LEQ 2022 (
+	touch "%%i" -c
 	) else (
 	echo 5. not meet touch condition
 	)
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MLLT
-MLLT - Machine Learning Library [T] 机器学习炼丹术
+@Copyright Tony Sensei from nowhere. Machine learning Alchemy. "If you are not part of the solution, you're part of the problem."
+MLLT - Machine Learning Lab [t]
